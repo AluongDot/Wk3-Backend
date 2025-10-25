780f27d0f8c065566e509338dbcdb6e582d760db
Aluong <aluongmadiir@gmail.com>
2025-10-25 10:04:33 +0300

    Initial commit for Wk3-Backend project

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const taskRoutes = require("./Routes/tasksRoutes");
+const connectDB = require("./config/db");
+const { default: axios } = require("axios");    
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5174",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ['Content-Type']
+}));
+app.use(express.json());
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("Task Manager API is running...");
+});
+
+// Use routes
+app.use("/api/tasks", taskRoutes);
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(` Server running on http://localhost:${PORT}`));
